9b57455c5f951a15c8ba76d7d75cd1c5f14e8a06
Jose Daniel <josebaduma@gmail.com>
2013-04-13 21:00:15 -0600

    Threads incluidos

--- iRobot/src/MainThreads.cpp
+++ iRobot/src/MainThreads.cpp
@@ -7,28 +7,45 @@
 
 #include "MainThreads.h"
 
+/**
+ * Constructor
+ */
 MainThreads::MainThreads()
 {
 	this->_mainThread = 0;
 }
 
+/**
+ * Destructor
+ */
 MainThreads::~MainThreads()
 {
-	free(this->_mainThread);
+	free(this->_mainThread); //Libera la memoria
 }
 
+/**
+ * Método utilizado para la llamada a la funcion que debe realizar
+ * el hilo de ejecucion aqui se determina que funcion debe realizar.
+ */
 void* MainThreads::functionThread(void* var)
 {
 	reinterpret_cast<MainThreads*>(var)->fThread();
 	return 0;
 }
 
+/**
+ * Funcionamiento del hilo de ejecución, en este metodo se declara
+ * todas las acciones que debe de ejecutarse en el inicio.
+ */
 void MainThreads::fThread()
 {
 
 }
 
+/**
+ * Inicio de las funciones que debe realizar los hilos de ejecucion.
+ */
 void MainThreads::start()
 {
-	pthread_create(this->_mainThread, 0, functionThread, this);
+	pthread_create(this->_mainThread, 0, MainThreads::functionThread, this);
 }
--- iRobot/src/MainThreads.h
+++ iRobot/src/MainThreads.h
@@ -10,16 +10,25 @@
 #include "pthread.h"
 #include <stdlib.h>
 
+/**
+ * Clase principal para el funcionamiento de los movientos de los individuos,
+ * usando hilos de ejecución para los cuales se haran las animaciones y otras
+ * funciones necesarias para el funcionamiento en conjunto de todos los elementos.
+ */
 class MainThreads
 {
 private:
+	//Hilo de ejecucion principal
 	pthread_t * _mainThread;
 
 public:
-	MainThreads();
-	virtual ~MainThreads();
+	MainThreads(); //Constructor
+	virtual ~MainThreads(); //Destructor
+	//Funcion hecha para el funcionamiento del hilo de ejecución
 	static void* functionThread(void* var);
+	//Funcionamiento de los hilos
 	void fThread();
+	//Ejecución de los hilos
 	void start();
 };
 
