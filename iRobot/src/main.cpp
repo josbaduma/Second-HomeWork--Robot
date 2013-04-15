67212ae0a566bf70cf00b949be06cfca50ec85f3
Jose Daniel <josebaduma@gmail.com>
2013-04-14 23:13:18 -0600

    Unas ultimas modificaciones

--- iRobot/src/main.cpp
+++ iRobot/src/main.cpp
@@ -7,6 +7,8 @@
 
 #include "PathFinding.h"
 #include "SimpleList.h"
+#include "ImageVideoDetection.h"
+#include <highgui.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -16,63 +18,109 @@ int LENGHTX = 20;
 int LENGHTY = 20;
 
 MatrixNode** matriz;
+ImageVideoDetection* detect;
 
 int getColor(int);
+void textImage();
+void textVideo();
 
 int main(int argc, char **argv)
 {
-	srand(time(NULL));
-	matriz = new MatrixNode*[LENGHTX * LENGHTY];
-	for (int i = 0; i < LENGHTY; ++i)
-	{
-		for (int j = 0; j < LENGHTX; ++j)
-		{
-			int r = getColor(rand() % 255);
-			int g = getColor(rand() % 255);
-			int b = getColor(rand() % 255);
-			cout << "Rojo: " << r << " Verde: " << g << " Azul: " << b << endl;
-			matriz[j + (LENGHTX * i)] = new MatrixNode(j, i, r, g, b);
-		}
-	}
+//	srand(time(NULL));
+//	matriz = new MatrixNode*[LENGHTX * LENGHTY];
+//	for (int i = 0; i < LENGHTY; ++i)
+//	{
+//		for (int j = 0; j < LENGHTX; ++j)
+//		{
+//			int r = getColor(rand() % 255);
+//			int g = getColor(rand() % 255);
+//			int b = getColor(rand() % 255);
+//			cout << "Rojo: " << r << " Verde: " << g << " Azul: " << b << endl;
+//			matriz[j + (LENGHTX * i)] = new MatrixNode(j, i, r, g, b);
+//		}
+//	}
+//
+//	MatrixNode* start = matriz[0 + (0 * LENGHTX)];
+//	MatrixNode* goal = matriz[19 + (11 * LENGHTX)];
+//
+//	PathFinding* pathfinding = new PathFinding(matriz, LENGHTX, LENGHTY);
+//
+//	cout << "Ejecutando" << endl;
+//
+//	SimpleList<MatrixNode*> * list = pathfinding->DistanceFirstSearch(start,
+//			goal);
+////	SimpleList<MatrixNode*> * list = pathfinding->AlgoritmInvented(start,goal);
+//
+//	cout << list->length() << endl;
+//	int tmp = list->length();
+//
+//	for (int i = 0; i < tmp; ++i)
+//	{
+//		MatrixNode* tmp2 = list->deleteFinal();
+//		cout << tmp2->getPosY() << "," << tmp2->getPosX() << endl;
+//	}
+//	cout << tmp << endl;
 
-	MatrixNode* start = matriz[0 + (0 * LENGHTX)];
-	MatrixNode* goal = matriz[19 + (11 * LENGHTX)];
-
-	PathFinding* pathfinding = new PathFinding(matriz, LENGHTX, LENGHTY);
-
-	cout << "Ejecutando" << endl;
-
-	SimpleList<MatrixNode*> * list = pathfinding->DistanceFirstSearch(start, goal);
-//	SimpleList<MatrixNode*> * list = pathfinding->AlgoritmInvented(start,goal);
-
-	cout << list->length() << endl;
-	int tmp = list->length();
-
-	for (int i = 0; i < tmp; ++i) {
-		MatrixNode* tmp2 = list->deleteFinal();
-		cout << tmp2->getPosY() << "," << tmp2->getPosX() << endl;
-	}
-	cout << tmp << endl;
-
+	textVideo();
 	return 0;
 }
 
-int getColor(int color) {
-	if (color < 32) {
+int getColor(int color)
+{
+	if (color < 32)
+	{
 		return 0;
-	} else if (color >= 32 && color < 64) {
+	}
+	else if (color >= 32 && color < 64)
+	{
 		return 1;
-	} else if (color >= 64 && color < 96) {
+	}
+	else if (color >= 64 && color < 96)
+	{
 		return 2;
-	} else if (color >= 96 && color < 128) {
+	}
+	else if (color >= 96 && color < 128)
+	{
 		return 3;
-	} else if (color >= 128 && color < 160) {
+	}
+	else if (color >= 128 && color < 160)
+	{
 		return 4;
-	} else if (color >= 160 && color < 192) {
+	}
+	else if (color >= 160 && color < 192)
+	{
 		return 5;
-	} else if (color >= 192 && color < 224) {
+	}
+	else if (color >= 192 && color < 224)
+	{
 		return 6;
-	} else {
+	}
+	else
+	{
 		return 7;
 	}
+}
+
+void textImage()
+{
+	detect = new ImageVideoDetection();
+	IplImage* img = cvLoadImage("image.jpg");
+	detect->matrixImage(img);
+	cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);
+	cvShowImage("Original", img);
+
+	cvNamedWindow("Reconstruccion", CV_WINDOW_AUTOSIZE);
+	cvShowImage("Reconstruccion", detect->reconstructImage());
+
+	cvWaitKey(100000);
+
+	cvReleaseImage(&img);
+	cvDestroyAllWindows();
+
+}
+
+void textVideo()
+{
+	detect = new ImageVideoDetection();
+	detect->videoMatrix(0);
 }
